7b25f675120f4cf6f7f3ee0243c60efa2431484f
invilso <40578604+invilso@users.noreply.github.com>
2023-06-01 02:51:46 +0300

    fix settings

--- cryptoarb/settings.py
+++ cryptoarb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 CSRF_TRUSTED_ORIGINS = ['https://*.invilso.pp.ua', 'http://*.invilso.pp.ua']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
